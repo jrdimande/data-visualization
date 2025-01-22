d1d55b07a8a3c8a7941b9038e74e57b4afeaef30
jrdimande <timoteojrdimande@gmail.com>
2025-01-22 03:09:06 +0200

    Data Visualization in python

---
+++ Generating Data/Matplotlib/Examples/Random Walk/rw_visual.py
@@ -0,0 +1,16 @@
+import matplotlib.pyplot as plt
+from random_walk import RandomWalk
+
+# Make a random walk
+rw = RandomWalk()
+rw.fill_walk()
+
+# Plot the points in the wal.
+plt.style.use('classic')
+
+fig, ax = plt.subplots()
+ax.scatter(rw.x_values, rw.y_values, s=15)
+plt.show()
+
+
+
