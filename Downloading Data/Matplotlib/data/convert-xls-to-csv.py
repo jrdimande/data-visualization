5eefd9c151c2219563527bd6a33003f01fccb819
jrdimande <timoteojrdimande@gmail.com>
2025-02-07 23:58:15 +0200

    Downloding data

--- Downloading Data/Matplotlib/data/convert-xls-to-csv.py
+++ Downloading Data/Matplotlib/data/convert-xls-to-csv.py
@@ -2,9 +2,9 @@ import pandas as pd
 def converter_xls_para_csv(xls_path, csv_path):
     df = pd.read_excel(xls_path)
     df.to_csv(csv_path, index=False)
-    print(f"Arquivo CSV salvo como {csv_path}")
+    print(f"Done {csv_path}")
 
-xls_path = "maputo_cidade.xls"
-csv_path = "seu_arquivo.csv"
+xls_path = ""
+csv_path = ""
 
 converter_xls_para_csv(xls_path, csv_path)
